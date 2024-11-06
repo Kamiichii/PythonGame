f967c0b188753b7b5ada041aff1fba99f4cad9a5
kamiichii <berkay.kilic.ce@gmail.com>
2024-11-06 23:03:29 +0300

    Added updateable and drawable conteiners

--- main.py
+++ main.py
@@ -8,21 +8,26 @@ from player import *
 def main():
     pygame.init()   
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
-
+    clock = pygame.time.Clock()    
     dt = 0
-
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable,drawable)
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-    
-        
+        for sth in updateable:
+            sth.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        for sth in drawable:
+            sth.draw(screen)
+        
         pygame.display.flip()
         
         dt = clock.tick(60)/1000
